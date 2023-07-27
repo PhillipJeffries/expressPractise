7a1a2331b462d44424ff239f63f95746efa8cff6
Dmitry <ggbox@mail.ru>
2023-07-27 08:29:57 +0400

    added type module in pacage json

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const express = require("express");
-// const cors = require("cors");
-// const router = require("./routes");
-// const AppError = require("./utils/appError");
-// const errorHandler = require("./utils/errorHandler");
+import express from "express";
 
 const app = express();
 const port = 3000;
