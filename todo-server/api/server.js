57a74197ca228fc605ba5a99c7920992070cb5fd
Dmitry <ggbox@mail.ru>
2023-08-19 22:13:38 +0400

    put delete create done

--- todo-server/api/server.js
+++ todo-server/api/server.js
@@ -22,20 +22,57 @@ server.get('/todos', async (req, res) => {
   } catch(err) {
     console.log(err);
   }
-
-  
 });
 
-server.post('/todos', (req, res) => {
-  
+server.get('/todos/:id', async (req, res) => {
+  const {id} = req.params;
+  try {
+    const currentTodo = await db('todos').where({ id });
+    currentTodo.length === 0 ? res.status(404).json({message: 'Todo not found'}) :
+    res.status(200).json(currentTodo);
+  } catch(err) {
+    console.log(err)
+  }
 });
 
-server.put('/todos/:id', (req, res) => {
   
+
+server.post('/todos', async (req, res) => {
+  const {message} = req.body;
+  console.log(message);
+  if(!message){
+    return res.status(400).json({message: 'You must include a message in your request'})
+  }
+  try {
+    await db('todos').insert({ message });
+    res.status(201).json({message: 'successfully'});
+  } catch(err) {
+    console.log(err)
+  }
 });
 
-server.delete('/todos/:id', (req, res) => {
-  
-})
+server.put('/todos/:id', async (req, res) => {
+  const {id} = req.params;
+  const { message } = req.body;
+  if(!message){
+    return res.status(400).json({message: 'You must include a message in your request'})
+  }
+  try {
+    await db('todos').where({id}).update({message});
+    res.status(200).json({message: 'Updated'});
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+server.delete('/todos/:id', async (req, res) => {
+  const {id} = req.params;
+  try {
+    await db('todos').where({id}).del();
+    res.status(200).json({message: 'Deleted'});
+  } catch(err) {
+    console.log(err);
+  }
+});
 
 module.exports = server;
